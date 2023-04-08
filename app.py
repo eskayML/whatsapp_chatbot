b0cb6ba6e5fd40145509f3ae37c828a888be747b
eskayML <kalusam619@gmail.com>
2023-04-08 15:40:38 +0100

    add: lt

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
 from twilio.rest import Client
+import config  ,cohere
 
 app = Flask(__name__)
 
+
 # Your Twilio account SID and auth token
-account_sid = 'your_account_sid'
-auth_token = 'your_auth_token'
+account_sid = config.ACCOUNT_SID
+auth_token = config.AUTH_TOKEN
 client = Client(account_sid, auth_token)
 
 # The WhatsApp phone number assigned to your Twilio account
@@ -26,21 +28,25 @@ def bot():
 
     # Create a response object
     resp = MessagingResponse()
+    print(incoming_msg)
+    
 
-    # If the user sends "hello", respond with a greeting
-    if 'hello' in incoming_msg:
-        resp.message('Hello! How can I assist you today?')
+    # # If the user sends "hello", respond with a greeting
+    # if 'hello' in incoming_msg:
+    #     resp.message('Hello! How can I assist you today?')
 
-    # If the user sends "bye", respond with a farewell
-    elif 'bye' in incoming_msg:
-        resp.message('Goodbye! Have a nice day.')
+    # # If the user sends "bye", respond with a farewell
+    # elif 'bye' in incoming_msg:
+    #     resp.message('Goodbye! Have a nice day.')
 
-    # If the user sends anything else, respond with an error message
-    else:
-        resp.message('I\'m sorry, I don\'t understand. Please try again.')
+    # # If the user sends anything else, respond with an error message
+    # else:
+    #     resp.message('I\'m sorry, I don\'t understand. Please try again.')
 
-    # Send the response back to the user
-    return str(resp)
+    # # Send the response back to the user
+    # return str(resp)
+
+
 
 
 DEBUG_MODE  = True
--- prompt.py
+++ prompt.py
@@ -2,8 +2,11 @@ import config, cohere
 
 co = cohere.Client(config.COHERE_API_KEY)
 
-chat_input = 'what is two hundred multiplied by twelve'
+chat_input = 'who is your creator'
+
+
 prompt = f"""
+You are an AI chatbot built by eskayML, your name is Chadbot
 Give a suitable reply to the statement below also making sure not to leave any sentence uncompleted
 {chat_input}
 """
@@ -15,6 +18,8 @@ response = co.generate(
     max_tokens=50,  
     frequency_penalty=.7, 
 )
+
+
 
 
 resp_text = response.generations[0].text
